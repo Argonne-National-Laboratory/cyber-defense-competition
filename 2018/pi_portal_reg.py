321a5d891e90887cf4363bb285e5e3923f90ea42
mt@trustdarkness.com <mt@trustdarkness.com>
2018-04-10 19:09:00 -0700

    add script for automatic registration of raspberry pis to the guest network

--- 2018/pi_portal_reg.py
+++ 2018/pi_portal_reg.py
@@ -4,6 +4,8 @@ import subprocess as sub
 import re
 from threading import Thread
 import argparse
+import time
+import requests
 
 class ThreadedRegisterMac(Thread):
     def __init__(self, mac, iface):
@@ -12,9 +14,23 @@ class ThreadedRegisterMac(Thread):
         self.iface = iface
 
     def run(self):
+        sub.check_call(['sudo', 'ifconfig', self.iface, 'down'])
         sub.check_call(['sudo', 'macchanger', '-m', self.mac, self.iface])
+        time.sleep(2)
+        sub.check_call(['sudo', 'ifconfig', self.iface, 'up'])
         sub.check_call(['sudo', 'dhclient', self.iface])
         # insert POST to captive portal here
+        action_url = "http://146.137.2.106/cgi-bin/register-new.cgi"
+        data = {"first": "Nataniel",
+                "last": "Evans",
+                "email": "nevans@anl.gov",
+                "building": "240",
+                "phone" : "6302523733",
+                "length":"week",
+                "":"submit"}
+        r = requests.post(action_url, data=data)
+        sub.Popen(['echo', r.text])
+
 
 def findWholeWord(w):
     return re.compile(r'\b({0})\b'.format(w), flags=re.IGNORECASE).search
@@ -26,14 +42,19 @@ if __name__ == '__main__':
     args = aparser.parse_args()
     macs = set()
     p = sub.Popen(('sudo', 'tcpdump', '-i', args.listen_iface, '-l', '-s 0', '-vvv', '-n', '((udp port 67) and (udp[8:1] = 0x1))'), stdout=sub.PIPE)
-    for row in iter(p.stdout.readline, b''):
-        if findWholeWord('requested-ip')(row):
-            mac = row.split(' ')[-1]
-            if mac not in macs:
-                macs.add(mac)
-                ThreadedRegisterMac(mac, args.talk_iface)
-        elif findWholeWord('client-id')(row):
-            mac = row.split(' ')[-1]
-            if mac not in macs:
-                macs.add(mac)
-                ThreadedRegisterMac(mac, args.talk_iface)
+    while True:
+        for row in iter(p.stdout.readline, b''):
+            if findWholeWord('requested-ip')(row):
+                mac = row.split(' ')[-1].strip()
+                if ":" in mac and len(mac) == 17 and mac not in macs:
+                    macs.add(mac)
+                    t = ThreadedRegisterMac(mac, args.talk_iface)
+                    print "registering %s to the guest network" % mac
+                    t.start()
+            elif findWholeWord('client-id')(row):
+                mac = row.split(' ')[-1].strip()
+                if ":" in mac and len(mac) == 17 and mac not in macs:
+                    macs.add(mac)
+                    t = ThreadedRegisterMac(mac, args.talk_iface)
+                    print "registering %s to the guest network" % mac
+                    t.start()
